fffb99eff6c3cca44a56028b5421bcc7b8b46d5b
Eliran Abuhatzira <elirana1989@gmail.com>
2022-06-14 11:01:14 +0300

    .

--- wp-content/themes/cymulate/functions.php
+++ wp-content/themes/cymulate/functions.php
@@ -49,9 +49,9 @@ function add_custom_taxonomies() {
   
 
   function wpdocs_theme_name_scripts() {
-    wp_enqueue_script( 'cymulate-script', get_template_directory_uri() . '/js/index.js', array( 'jquery' ), '1.0.0', false );
-    wp_enqueue_script( 'weather-script', get_template_directory_uri() . '/js/weather.js', array( 'jquery' ), '1.0.0', false );
-    wp_enqueue_style( 'cymulate-style', get_template_directory_uri()."/css/index.css" );
+    wp_enqueue_script( 'cymulate-script', get_template_directory_uri() . '/js/index.js', array( 'jquery' ), '1.0.2', false );
+    wp_enqueue_script( 'weather-script', get_template_directory_uri() . '/js/weather.js', array( 'jquery' ), '1.0.2', false );
+    wp_enqueue_style( 'cymulate-style', get_template_directory_uri()."/css/index.css"  , array() , '1.0.2');
 }
 
   add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
